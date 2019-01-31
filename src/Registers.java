c0f2135cc231307746ce55a1aa202df526c3c56f
ArtinTD <artin.taj@gmail.com>
2019-01-31 18:45:58 +0330

    Change register methods and fields to nonstatic

--- src/Registers.java
+++ src/Registers.java
@@ -2,11 +2,11 @@
  * Created by mhebt on 29/01/2019.
  */
 public class Registers {
-    private static int[] registers = new int[32];
-    static int read(int index){
+    private int[] registers = new int[32];
+    int read(int index){
         return registers[index];
     }
-    static void write(int index,int value){
+    void write(int index,int value){
         registers[index]= value;
     }
 }
