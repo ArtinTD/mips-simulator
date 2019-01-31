1de6d951e03c3d678a5deab4eda20a9e12b3c120
ArtinTD <artin.taj@gmail.com>
2019-01-31 19:10:28 +0330

    Complete compute method of ALU

--- src/ALU.java
+++ src/ALU.java
@@ -8,37 +8,46 @@ public class ALU {
             case ADD:
                 int reg1 = registers.read( instruction.Rs );
                 int reg2 = registers.read( instruction.Rt );
+                registers.write(instruction.Rd, reg1 + reg2);
                 return reg1+ reg2;
             case SUB:
                 reg1= registers.read( instruction.Rs );
                 reg2 = registers.read( instruction.Rt );
+                registers.write(instruction.Rd, reg1 - reg2);
                 return reg1 - reg2;
             case AND:
                 reg1 = registers.read( instruction.Rs );
                 reg2 = registers.read( instruction.Rt );
+                registers.write(instruction.Rd, reg1 & reg2);
                 return reg1 & reg2;
             case OR:
                 reg1 = registers.read( instruction.Rs );
                 reg2 = registers.read( instruction.Rt );
+                registers.write(instruction.Rd, reg1 | reg2);
                 return reg1 | reg2;
             case NOR:
                 reg1 = registers.read( instruction.Rs );
                 reg2 = registers.read( instruction.Rt );
+                registers.write(instruction.Rd, ~(reg1 | reg2));
                 return ~(reg1 | reg2);
             case SLT:
                 reg1 = registers.read( instruction.Rs );
                 reg2 = registers.read( instruction.Rt );
+                registers.write(instruction.Rd, (reg1 <= reg2 ? 0 : 1));
                 return (reg1 <= reg2 ? 0 : 1);
             case BEQ:
                 reg1 = registers.read( instruction.Rs );
                 reg2 = registers.read( instruction.Rt );
                 return (reg1 == reg2 ? 1 : 0);
             case LW:
-                // TODO: 1/31/2019
+                reg1 = registers.read( instruction.Rs );
+                int imd = instruction.immediate;
+                return reg1 + imd;
             case SW:
-                // TODO: 1/31/2019
+                reg1 = registers.read( instruction.Rs );
+                imd = instruction.immediate;
+                return reg1 + imd;
         }
-
         return -1;
     }
 
